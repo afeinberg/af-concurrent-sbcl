905e81616f7fc18be283e1ce2c9e255c6ed6d335
Alex Feinberg <alex@strlen.net>
2011-02-06 23:56:46 -0800

    Cite original sources for util/queue.lisp

--- src/util/queue.lisp
+++ src/util/queue.lisp
@@ -1,3 +1,12 @@
+;; Packaged version of singly-linked list based FIFO queue described
+;; by Paul Graham in Ansi Common Lisp. Functions names changed not to
+;; conflict with the lock free queue in sb-concurrency.
+
+;; Taken from
+;; http://cs.northwestern.edu/academics/courses/325/programs/graham.lisp
+
+;; Changes from original version: fixed a leak found by Matt Knox. 
+
 (in-package :af-concurrency.util)
 
 (defun af/make-queue () (cons nil nil))
